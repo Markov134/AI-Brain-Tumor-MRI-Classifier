1f1b74f134574cf7eec9a1e322301baaf0248d0c
Markov134 <stephanmarokvski@gmail.com>
2026-08-06 18:58:37 +0200

    Added an evaluation python file. Changed code in main.

---
+++ src/evaluate.py
@@ -0,0 +1,100 @@
+import torch
+
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report,
+    confusion_matrix
+)
+
+from preprocessing import load_metadata, create_dataloaders
+from model import create_model
+
+def evaluate_model():
+    """Evaluate the trained ResNet-18 model."""
+
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )
+
+    print(f"Using device: {device}")
+    
+    metadata = load_metadata()
+    _, test_loader = create_dataloaders(metadata)
+    
+    model = create_model()
+
+    model.load_state_dict(
+        torch.load(
+            "models/resnet18_brain_tumor.pth",
+            map_location=device
+        )
+    )
+
+    model = model.to(device)
+    
+    model.eval()
+
+    all_predictions = []
+
+    all_labels = []
+
+    with torch.no_grad():
+
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+
+            _, predicted_classes = torch.max(outputs, dim=1)
+
+            all_predictions.append(predicted_classes)
+
+            all_labels.append(labels)
+
+    all_predictions = torch.cat(all_predictions)
+    all_labels = torch.cat(all_labels)
+
+    accuracy = accuracy_score(all_labels, all_predictions)
+
+    precision = precision_score(
+        all_labels,
+        all_predictions,
+        average = "weighted"
+    )
+
+    recall = recall_score(
+        all_labels,
+        all_predictions,
+        average = "weighted"
+    )
+
+    f1 = f1_score(
+        all_labels,
+        all_predictions,
+        average = "weighted"
+    )
+
+    matrix = confusion_matrix(
+        all_labels,
+        all_predictions
+    )
+
+    print("\nAccuracy :", round(accuracy, 4))
+    print("Precision:", round(precision, 4))
+    print("Recall   :", round(recall, 4))
+    print("F1 Score :", round(f1, 4))
+    print("Confusion Matrix:", matrix)
+
+    print("\nClassification Report:\n")
+    print(classification_report(all_labels, all_predictions))
+        
+
+if __name__ == "__main__":
+    evaluate_model()
+
+
+
--- src/main.py
+++ src/main.py
@@ -2,18 +2,12 @@ from database import *
 from preprocessing import *
 from model import *
 from train import *
+from evaluate import *
 
 def main():
-    metadata = load_metadata()
-    train_loader, test_loader = create_dataloaders(metadata)
 
-    print(len(train_loader))
-    print(len(test_loader))
-
-    model = create_model()
-    print(model)
-
-    trained_model = train_model()
+    evaluate_model()
+    
 
 if __name__ == "__main__":
     main()
